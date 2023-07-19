a4a4187f15a55c031f13605c1185216dd120e7d0
danielmegale <danim1@live.it>
2023-07-19 14:24:16 +0200

    added random pass

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-$lengh_pass = $_GET['pass'];
+$length_pass = $_GET['pass'] ?? 0;
+$characters = 'abcdefghijklmnopqrstvuwyxzABCDEFGHIJKLMNOPQRSTVUWYXZ0123456789!@#$%^&*';
+function random_pass($length_pass, $characters)
+{
+    return substr(str_shuffle($characters), 0, $length_pass);
+};
+var_dump(random_pass($length_pass, $characters));
 ?>
 
 
